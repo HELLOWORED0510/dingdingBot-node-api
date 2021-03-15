0d96ff1c31305dc58093b17c47311fdc29234096
yinjikai <1259559060@qq.com>
2021-03-15 22:09:42 +0800

    build: 指定包范围

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,15 @@
 declare class DingDingHelper {
-  secret: string
-  url: string
-  token: string
-  constructor(opts: { secret: string; token: string })
-  generateSign: (
-    secret: any
-  ) => {
-    dateNow: number
-    result: string
-  }
-  sendMessage: (text: string, desp: string) => void
+    secret: string;
+    url: string;
+    token: string;
+    constructor(opts: {
+        secret: string;
+        token: string;
+    });
+    generateSign: (secret: any) => {
+        dateNow: number;
+        result: string;
+    };
+    sendMessage: (text: string, desp: string) => void;
 }
-export default DingDingHelper
+export default DingDingHelper;
